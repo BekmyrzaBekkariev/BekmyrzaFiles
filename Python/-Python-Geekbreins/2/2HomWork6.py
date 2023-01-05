4277e0e4d9df10cc5e3fcfe887ccea51ed6044c3
Bekmyrza <bekmyrzabekkariev05@gmail.com>
2023-01-06 01:54:36 +0600

    fire3

---
+++ Python/-Python-Geekbreins/2/2HomWork6.py
@@ -0,0 +1,15 @@
+import random
+
+num = random.randint(0, 100)
+print("Отгадать число от 0 до 100 за 10 попыток")
+for i in range(1, 11):
+    answer = int(input(f'Попытка {i}: '))
+    if num < answer:
+        print("Число меньше")
+    elif num > answer:
+        print("Число больше")
+    else:
+        print(f"Вы угадали с {i}-й попытки")
+        break2
+else:
+    print(f" Поражение. Было загадано {num}")
